d9b7c1b822cbe5961fc249e72eedc020aa77351c
sq591442679 <591442679@qq.com>
2023-09-14 14:47:15 +0800

    log priority

--- container_helper_scripts/log.sh
+++ container_helper_scripts/log.sh
@@ -2,8 +2,11 @@
 
 # touch /var/log/frr/sqsq_ospfd.log
 # chomd 777 /var/log/frr/sqsq_ospfd.log
+# NOTE do not new the log file because frr does it automatically
+# NOTE it seems frr can only write logs to /var/log/frr/
 
 echo "
 log file /var/log/frr/sqsq_ospfd.log
+log record-priority
 " >> /etc/frr/frr.conf
 
